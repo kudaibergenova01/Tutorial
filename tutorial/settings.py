87d7fae0280b913e955a7c4abddac853f35e4d81
kudaibergenova01 <kudaibergenovab01@gmail.com>
2021-08-11 12:58:41 +0600

    Requests and Responses

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-from distutils.command.config import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +19,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '1*_2d($dtxg_c^^hstu%igj_w4828_n8r=zm)*^r+3dompl10l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
